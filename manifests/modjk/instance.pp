e369543f958a67df80428644f7102f8ba4eff44f
Romain THERRAT <romain42@gmail.com>
2015-06-25 12:25:15 +0200

    Add: modjk instance connection_pool_timeout parameter

--- manifests/modjk/instance.pp
+++ manifests/modjk/instance.pp
@@ -33,17 +33,26 @@
 # [*fail_on_status*]
 #  The http status code to return when an error occured.
 #  Default: 0
+#
+# [*connection_pool_timeout*]
+#   Cache timeout property should be used with connection_pool_minsize
+#   to specify how many seconds JK should keep an inactive socket in cache
+#   before closing it. This property should be used to reduce the number of
+#   threads on the Tomcat web server. The default value zero disables the
+#   closing (infinite timeout).
+#   Default : 600 ( = 10minutes)
 define tomcat::modjk::instance (
   $workers_file,
   $ajp_port,
-  $instance_name  = $name,
-  $host           = 'localhost',
-  $type           = 'ajp13',
-  $lbfactor       = '1',
-  $ping_mode      = '',
-  $socket_timeout = '0',
-  $domain         = '',
-  $fail_on_status = '0',
+  $instance_name           = $name,
+  $host                    = 'localhost',
+  $type                    = 'ajp13',
+  $lbfactor                = '1',
+  $ping_mode               = '',
+  $socket_timeout          = '0',
+  $domain                  = '',
+  $fail_on_status          = '0',
+  $connection_pool_timeout = '600',
 ) {
     concat::fragment{"tomcat_modjk_instance_${name}":
       target  => $workers_file,
